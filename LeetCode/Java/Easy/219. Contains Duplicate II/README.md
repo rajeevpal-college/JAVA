ae8d2a3204bafd3c1612d278765f81390b6ee2d1
rajeevpal-college <800rajeevpal@gmail.com>
2026-09-18 00:51:28 +0530

    Add Easy Java Solution for 219. Contains Duplicate II from LeetCode

--- LeetCode/Java/Easy/219. Contains Duplicate II/README.md
+++ LeetCode/Java/Easy/219. Contains Duplicate II/README.md
@@ -8,8 +8,8 @@
 Array, Hash Table, Sliding Window
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 29 ms
+- **Memory:** 125 MB
 
 ---
 
--- LeetCode/Patterns/Sliding Window/219. Contains Duplicate II/Solution.java
+++ LeetCode/Patterns/Sliding Window/219. Contains Duplicate II/Solution.java
@@ -1,17 +1,24 @@
+import java.util.HashMap;
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        //int j=nums.length-1;
-        for(int i=0;i<nums.length;i++){
-            for (int j=0;j<nums.length;j++){
-            if(i!=j&&nums[i]==nums[j]&&(Math.abs(i-j)<=k)){
-            //((i-j<=k)||(j-i<=k))){   //((i-j<=k)&&(j-i>=k))){
-                return true;}
-                //j--;
-            
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(nums[i])) {
+
+                int prevIndex = map.get(nums[i]);
+
+                if (i - prevIndex <= k) {
+                    return true;
+                }
+            }
+
+            map.put(nums[i], i);
         }
-        }
+
         return false;
-        
-        
     }
 }
