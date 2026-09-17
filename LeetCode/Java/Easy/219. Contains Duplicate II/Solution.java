e1969836a5da84bb85249401ebdc3b4c49aef67f
rajeevpal-college <800rajeevpal@gmail.com>
2026-09-17 23:43:24 +0530

    Add Easy Java Solution for 219. Contains Duplicate II from LeetCode

--- LeetCode/Java/Easy/219. Contains Duplicate II/Solution.java
+++ LeetCode/Java/Easy/219. Contains Duplicate II/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         //int j=nums.length-1;
         for(int i=0;i<nums.length;i++){
             for (int j=0;j<nums.length;j++){
-            if(nums[i]==nums[j]&&((i-j<=k)||(j-i<=k))){
+            if(nums[i]==nums[j]&&((i-j<=k)||(j-i>=k))){
                 return true;}
                 //j--;
             
