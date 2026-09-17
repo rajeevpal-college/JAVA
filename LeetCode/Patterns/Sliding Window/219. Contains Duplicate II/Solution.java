3bdb7857a523c0d61021334d91202264481e5a77
rajeevpal-college <800rajeevpal@gmail.com>
2026-09-17 23:27:59 +0530

    Add Easy Java Solution for 219. Contains Duplicate II from LeetCode

---
+++ LeetCode/Patterns/Sliding Window/219. Contains Duplicate II/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        //int j=nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            for (int j=0;j<nums.length;j++){
+            if(nums[i]==nums[j]&&i-j==k){
+                return true;}
+                //j--;
+            
+        }
+        }
+        return false;
+        
+        
+    }
+}
